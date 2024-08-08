15c42e6ad02a4093f71486f14481a3f3a21bda00
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-08-08 22:19:43 +0200

    works for newer versions now

--- FormatPlugin/src/main/java/me/lukasabbe/format/Format.java
+++ FormatPlugin/src/main/java/me/lukasabbe/format/Format.java
@@ -1,13 +1,23 @@
 package me.lukasabbe.format;
 
+import me.lukasabbe.format.commands.CommandFormat;
+import me.lukasabbe.format.commands.CommandTool;
+import me.lukasabbe.format.commands.TabCompleteCommandFormat;
+import me.lukasabbe.format.events.EventAxe;
+import me.lukasabbe.format.objects.BoxValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public final class Format extends JavaPlugin {
+    public HashMap<UUID, BoxValue> boxValues;
     @Override
     public void onEnable() {
+        boxValues = new HashMap<>();
         this.getCommand("formattool").setExecutor(new CommandTool());
-        this.getCommand("format").setExecutor(new CommandFormat());
+        this.getCommand("format").setExecutor(new CommandFormat(boxValues));
         this.getCommand("format").setTabCompleter(new TabCompleteCommandFormat());
-        this.getServer().getPluginManager().registerEvents(new EventAxe(), this);
+        this.getServer().getPluginManager().registerEvents(new EventAxe(boxValues), this);
     }
 }
